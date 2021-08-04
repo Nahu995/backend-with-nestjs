eb532ccf1f496387baedd97c31fa2b41e79a3daa
Nahuel Torres <nahu995@gmail.com>
2021-08-04 00:12:07 -0300

    controllers - single responsibility

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,40 +4,4 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-  @Get('nuevo')
-  newEndpoint(): string {
-    return `this.appService.getHello()`;
-  }
-
-  @Get('products')
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ): string {
-    return `products: limit => ${limit} offset => ${offset} brand => ${brand}`;
-  }
-
-  @Get('products/filter')
-  getFilter(): string {
-    return `filter`;
-  }
-
-  @Get('products/:productId')
-  getProduct(@Param('productId') productId: string): string {
-    return `product ${productId}`;
-  }
-
-  @Get('categories/:id/products/:productId')
-  getCategory(
-    @Param('id') id: string,
-    @Param('productId') productId: string,
-  ): string {
-    return `product ${productId} and ${id}`;
-  }
 }
